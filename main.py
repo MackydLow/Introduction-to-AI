14e9966f88553312220c5d539c6ad88dc41d35be
MackydLow <2636498@dundee.ac.uk>
2025-11-13 17:13:09 +0000

    save

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
+
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-
+ 
 # load the dataset, split into input (X) and output (y) variables
-dataset = np.loadtxt('pima-indians-diabetes.csv', delimiter=',')
+dataset = np.loadtxt('pima-indians-diabetes.data.txt', delimiter=',')
 X = dataset[:,0:8]
 y = dataset[:,8]
-
-#convert to PyTorch tensor 
+ 
 X = torch.tensor(X, dtype=torch.float32)
 y = torch.tensor(y, dtype=torch.float32).reshape(-1, 1)
-
-#Define Model
+ 
+# define the model
 model = nn.Sequential(
     nn.Linear(8, 12),
     nn.ReLU(),
@@ -21,6 +21,33 @@ model = nn.Sequential(
     nn.Linear(8, 1),
     nn.Sigmoid()
 )
-
-
 print(model)
+ 
+# train the model
+loss_fn   = nn.BCELoss()  # binary cross entropy
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+ 
+n_epochs = 100
+batch_size = 10
+ 
+for epoch in range(n_epochs):
+    for i in range(0, len(X), batch_size):
+        Xbatch = X[i:i+batch_size]
+        y_pred = model(Xbatch)
+        ybatch = y[i:i+batch_size]
+        loss = loss_fn(y_pred, ybatch)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    print(f'Finished epoch {epoch}, latest loss {loss}')
+ 
+# compute accuracy (no_grad is optional)
+with torch.no_grad():
+    y_pred = model(X)
+accuracy = (y_pred.round() == y).float().mean()
+print(f"Accuracy {accuracy}")
+
+# make class predictions with the model
+predictions = (model(X) > 0.5).int()
+for i in range(5):
+    print('%s => %d (expected %d)' % (X[i].tolist(), predictions[i], y[i]))
