c61e47e35042c048f22561db76093a5fcccfd9b1
MackydLow <2636498@dundee.ac.uk>
2025-11-30 19:53:59 +0000

    save

---
+++ final-project/policyModel.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+import torch.nn.functional as nnf
+
+class PolicyModel(nn.Module):
+    def__init__(self, inputChannels, hiddenDim, outputDim, droupout):
+        inputChannels = 1
+        hiddenDim = 128
+        outputDim = 4
+        dropout = 0.1
+        super().__init__()
+    
+def main():
+    print("hii")
+
+if __name__ == "__main__":
+    main()
